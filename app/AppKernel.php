38329ffde485385fa573b35953f0e8578ffbb4a1
Nek <nek.dev@gmail.com>
2012-04-20 11:22:46 +0200

    Mistake in the bundle name corrected

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new Hermes\Bundle\HermesBundle\FrequenceWebHermesBundle(),
+            new Hermes\Bundle\HermesBundle\HermesHermesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
