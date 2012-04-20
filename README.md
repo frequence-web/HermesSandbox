27979ef129d6012cc7c04ece1dbc71a55b651912
Nek <nek.dev@gmail.com>
2012-04-20 10:17:25 +0200

    Addition of the InterfaceController

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Hermes Standard Edition
 
 Welcome to the Hermes Standard Edition - a fully-functional newletter system using Symfony2.
 
+See the documentation of HermesBundle for more informations
+
 1) What is it ?
 --------------------------------
 
-1) What it use ?
+2) What it use ?
 --------------------------------
--- src/FrequenceWeb/Bundle/HermesBundle/Controller/InterfaceController.php
+++ src/FrequenceWeb/Bundle/HermesBundle/Controller/InterfaceController.php
@@ -5,11 +5,11 @@ namespace FrequenceWeb\Bundle\HermesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class DefaultController extends Controller
+class InterfaceController extends Controller
 {
     
     public function indexAction($name)
     {
-        return $this->render('FrequenceWebHermesBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('FrequenceWebHermesBundle:Interface:index.html.twig', array('name' => $name));
     }
 }
